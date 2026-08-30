ec9f3ed87c6a30a545d0b935a860aac05a1e22d9
Cristian Radulescu <kristianradulescu@gmail.com>
2026-08-30 14:47:37 +0200

    feat: written healthCheck as a basic GET template
    Written a type serializer-like function for printing a struct with JSON indent.

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/KrisXVII/beacon
+
+go 1.26.3
---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Event struct {
+	Message string `json:"message"`
+	Number  int    `json:"number"`
+}
+
+func (e Event) String() string {
+	b, _ := json.MarshalIndent(e, "", "  ")
+	return string(b)
+}
+
+// Function that returns nothing
+func healthCheck(w http.ResponseWriter, r *http.Request) { // w is where the response is written, r the incoming request
+	e := Event{Message: "Hello there", Number: 43}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	//w.Write([]byte(`{"status":"ok"}`)) // Write deals in bytes, string is converted to bytes here
+	fmt.Println(e)
+	if e.Message == "" {
+		http.Error(w, "message required", http.StatusBadRequest)
+		return
+	}
+	err := json.NewEncoder(w).Encode(e)
+	if err != nil {
+		return
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()                   // registry of routes
+	mux.HandleFunc("GET /healthz", healthCheck) // register the route, map path to function
+	log.Println("Beacon listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", mux)) // ListenAndServe exposes the port
+}
